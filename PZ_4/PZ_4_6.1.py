0c12ac0f5be4f448787bc69cf987530f7973d827
AnastasiaGlobina <145655692+AnastasiaGlobina@users.noreply.github.com>
2023-11-16 18:36:54 +0300

    Add files via upload

---
+++ PZ_4/PZ_4_6.1.py
@@ -0,0 +1,12 @@
+number = int(input ("введите первое число: "))
+numbers = int(input ("введите второе число: "))
+sum = 0
+if number < numbers:
+    for i in range(number, numbers + 1):
+        sum += i
+    123
+    1234
+    print ("Сумма чисел от", number, "до", numbers, "равна:", sum)
+else:
+    print ("number больше numbers ")
+
---
+++ PZ_4/PZ_4_6.2.py
@@ -0,0 +1,2 @@
+number = int(input ("введите первое число: "))
+numbers = int(input ("введите второе число: "))
