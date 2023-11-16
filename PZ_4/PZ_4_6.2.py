dfd012c79b25ce6de3bd1150a7344286b80507fe
AnastasiaGlobina <145655692+AnastasiaGlobina@users.noreply.github.com>
2023-11-16 19:06:37 +0300

    Update PZ_4_6.2.py

--- PZ_4/PZ_4_6.2.py
+++ PZ_4/PZ_4_6.2.py
@@ -1,2 +1,8 @@
-number = int(input ("введите первое число: "))
-numbers = int(input ("введите второе число: "))
+number = int(input ("введите целлое число: "))
+digits = []
+while number >0:
+    digit = number % 10
+    digits.insert (0, digit)
+    number //=10
+for digit in digits:
+    print (digit)
