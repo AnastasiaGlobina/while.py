351a2ce6db15fe5f06a05efd1720ddf9d5298fb4
AnastasiaGlobina <145655692+AnastasiaGlobina@users.noreply.github.com>
2023-11-27 17:09:03 +0300

    Update PZ_4_6.2.py

--- PZ_4/PZ_4_6.2.py
+++ PZ_4/PZ_4_6.2.py
@@ -1,8 +1,9 @@
-number = int(input ("введите целлое число: "))
-digits = []
-while number >0:
-    digit = number % 10
-    digits.insert (0, digit)
-    number //=10
-for digit in digits:
-    print (digit)
+#Код разбивает введенное пользоавателем число на отдельные цифры и выводит их в обратном порядке
+try:
+    number = int(input("введите целое число: "))
+    while number > 0:
+        digit = number % 10
+        print(digit)
+        number = number//10
+except ValueError:
+    print("Вы ввели не чило, попытайтесь ввести цифры!")
