0ffd7b07e2934c87d66df4219ff289e46e2f66f3
AnastasiaGlobina <145655692+AnastasiaGlobina@users.noreply.github.com>
2023-12-07 18:44:46 +0300

    Update PZ_5_6.2.py

--- PZ_5/PZ_5_6.2.py
+++ PZ_5/PZ_5_6.2.py
@@ -2,14 +2,10 @@
 #положительного числа K на обратный (K — параметр целого типа, являющийся
 #одновременно входным и выходным). С помощью этой функции поменять порядок
 #следования цифр на обратный для каждого из пяти данных целых чисел.
-def Digits(K):
-    reversed_number = 0
-    while K > 0:
-        digit = K %10
-        reversed_number = (reversed_number*10) + digit
-        K//=10
-        return reversed_number
-numbers = [12,45,67,90,23]
-for num in numbers:
-    reversed_num = Digits(num)
-    print (reversed_num)
+def Digits(numbers):
+    mas= []
+    for num in numbers:
+        mas.append(num%10*10 + num//10)
+    return mas
+numbers = [12, 45, 67, 90, 23]
+print(Digits(numbers))
